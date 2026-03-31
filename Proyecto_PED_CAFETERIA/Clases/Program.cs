0e198e57ad27997bbe2074ab197af12023b3ee39
geo-lo <lgeovany269@gmail.com>
2026-03-31 13:45:21 -0600

    Change startup form from frmLogin to FrmVistaUsuario
    el boton entrar del formulario login no funciona XD

--- Proyecto_PED_CAFETERIA/Clases/Program.cs
+++ Proyecto_PED_CAFETERIA/Clases/Program.cs
@@ -17,7 +17,7 @@ namespace CAFETERIA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new FrmVistaUsuario());
         }
     }
 }
