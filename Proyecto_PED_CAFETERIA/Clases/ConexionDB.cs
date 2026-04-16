adf3d635f215cbc5e7d50d28c8226acfdd3de549
CSSC-2007 <christophercalles0307@gmail.com>
2026-04-16 17:35:33 -0600

    v1.0.1 Se agregaron 2 clases nuevas:
    *ConsultasDB: clase en donde se encuentran las funciones para agregar, modificar, eliminar y mostrar
    
    *ConexionDB: contiene funcion  de creacion y cierre de conexion a la database
    
    IMPORTANTE: en la cadena de conexion al inicio de ConexionDB se debe de cambiar el nombre del servidor "LAPODEROSA" por el nombre del servidor de sus ordenadores

---
+++ Proyecto_PED_CAFETERIA/Clases/ConexionDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient; //libreria necesaria para db
+
+namespace Proyecto_PED_CAFETERIA.Clases
+{
+    internal class ConexionDB
+    {
+        //cadena de conexion a la base de datos
+        private readonly string connectionString = "Server=LAPODEROSA; Database=CafeteriaDB; Integrated Security=True;";
+
+        //objeto de conexion
+        protected SqlConnection connection;
+
+        //Metodo para obtenr la conexion a la base de datos
+        public SqlConnection AbrirConexion()
+        {
+            connection = new SqlConnection(connectionString);
+
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+
+        //Metodo para cerrar la conexion a la base de datos
+        public void CerrarConexion()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+                connection.Dispose(); //Liberar recursos
+            }
+
+        }
+    }
+}
---
+++ Proyecto_PED_CAFETERIA/Clases/ConsultasDB.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_PED_CAFETERIA.Clases
+{
+    internal class ConsultasDB : ConexionDB
+    {
+
+        //Obtener datos (Select) de la tabla Inventario
+        public DataTable MostrarInventario()
+        {
+            DataTable tabla = new DataTable();
+            SqlCommand comando = new SqlCommand();
+
+            comando.Connection = AbrirConexion();
+            comando.CommandText = "SELECT IdProducto, NombreProducto, CantidadActual, PrecioUnitario FROM Inventario"; //Consulta SQL para obtener todos los productos
+
+            SqlDataReader leer = comando.ExecuteReader();
+            tabla.Load(leer); //Cargar los datos en la tabla
+
+            leer.Close();
+            CerrarConexion();
+            return tabla;
+        }
+
+        //Insertar datos (Insert) en la tabla Inventario
+        public void Insertar(string nombre, int cantidad, double precio )
+
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = AbrirConexion();
+            comando.CommandText = "INSERT INTO Inventario (NombreProducto, CantidadActual, PrecioUnitario) VALUES (@nombre, @cantidad, @precio)"; //Consulta SQL para insertar un nuevo producto
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@cantidad", cantidad);
+            comando.Parameters.AddWithValue("@precio", precio);
+            
+            comando.ExecuteNonQuery();
+            CerrarConexion();
+        }
+
+        //Actualizar datos (Update) en la tabla Inventario
+        public void EditarProducto(string nombre, int cantidad, double precio)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = AbrirConexion();
+
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@cantidad", cantidad);
+            comando.Parameters.AddWithValue("@precio", precio);
+
+            comando.ExecuteNonQuery();
+            CerrarConexion() ;
+        }
+
+        //Eliminar datos (Delete) de la tabla Inventario
+        public void EliminarProducto(string nombre)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = AbrirConexion();
+            comando.CommandText = "DELETE FROM Inventario WHERE NombreProducto = @nombre"; //Consulta SQL para eliminar un producto por su nombre
+
+            comando.Parameters.AddWithValue("@nombre", nombre);
+
+            comando.ExecuteNonQuery();
+            CerrarConexion();
+        }
+    }
+}
